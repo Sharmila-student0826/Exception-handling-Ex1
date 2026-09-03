5aa482e51219ca7a3a62d92c075a8ebebabc7176
Sharmila-student0826 <nsharmila0826@gmail.com>
2026-09-03 12:58:35 +0530

    Exception handling Ex4

---
+++ Ex4.java
@@ -0,0 +1,19 @@
+//throw key word
+class Ex4{
+void div(int a,int b){
+try{
+System.out.println(a/b);
+}catch(ArithmeticException e1){
+System.out.println("div():divide by zero -exception"+e1);
+throw e1;
+}}
+public static void main(String[] ar){
+int a=10;
+int b=2;//if zero it will throw ArithmeticException
+Ex4 e=new Ex4();
+try{
+e.div(a,b);
+}catch(ArithmeticException ee){
+
+System.out.println("main():divide by zero -exception"+ee);
+}}}
